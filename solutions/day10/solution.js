bb1b837a3b2407136cd156164c265165d77d7161
John Beech <johnbeech.github@mkv25.net>
2022-12-10 07:42:11 +0000

    Solve day 10 for second star

--- solutions/day10/solution.js
+++ solutions/day10/solution.js
@@ -1,5 +1,5 @@
 const path = require('path')
-const { read, position } = require('promise-path')
+const { read, write, position } = require('promise-path')
 const fromHere = position(__dirname)
 const report = (...messages) => console.log(`[${require(fromHere('../../package.json')).logName} / ${__dirname.split(path.sep).pop()}]`, ...messages)
 
@@ -98,8 +98,113 @@ async function solveForFirstStar (input) {
   report('Solution 1:', solution, { context })
 }
 
+function createEmptyGrid (width, height, symbol = ' ') {
+  const result = []
+  while (result.length < height) {
+    const row = []
+    while (row.length < width) {
+      row.push(symbol)
+    }
+    result.push(row)
+  }
+  return result
+}
+
+function gridToString (input) {
+  let result = ''
+  input.forEach(row => {
+    result += row.join('') + '\n'
+  })
+  return result
+}
+
 async function solveForSecondStar (input) {
-  const solution = 'UNSOLVED'
+  const commands = [{
+    cycleTime: 1,
+    regex: /noop/,
+    fn: (context) => {
+      console.log('No op')
+      return context
+    }
+  }, {
+    cycleTime: 2,
+    regex: /add([a-z]) (-?\d+)/,
+    fn: (context, register, value) => {
+      context.registers[register] += Number.parseInt(value)
+      console.log('Increased', { register }, 'by', { value }, { registers: context.registers, cycles: context.cycles })
+      return context
+    }
+  }, {
+    cycleTime: 0,
+    regex: /.*/,
+    fn: (context, any) => {
+      console.error('Unmatched command', { any })
+      return context
+    }
+  }]
+  const instructions = parseInstructions(input, commands)
+  const context = {
+    cycles: 0,
+    signalStrengths: [],
+    registers: { x: 1 },
+    executionMap: {},
+    screen: createEmptyGrid(40, 6, '.')
+  }
+
+  while (context.cycles < 220) {
+    instructions.reduce((context, instruction) => {
+      // register future commands
+      const [, ...commandArgs] = instruction.matches
+      const futureCycleTime = context.cycles + instruction.command.cycleTime
+      const futureActions = context.executionMap[futureCycleTime] ?? []
+      const futureCommand = (context) => {
+        return instruction.command.fn(context, ...commandArgs)
+      }
+      futureActions.push(futureCommand)
+      context.executionMap[futureCycleTime] = futureActions
+
+      // update cycle count
+      let n = 0
+      while (n < instruction.command.cycleTime) {
+        // execute current commands
+        const nowActions = context.executionMap[context.cycles] ?? []
+        context = nowActions.reduce((context, action) => {
+          return action(context)
+        }, context)
+        delete context.executionMap[context.cycles]
+
+        context.cycles++
+
+        // draw pixel to screen
+        const spritePosition = context.registers.x
+        const pixelColumn = (context.cycles - 1) % 40
+        const pixelRow = Math.floor(((context.cycles - 1) % 240) / 40)
+        const pixelOn = (pixelColumn >= spritePosition - 1 && pixelColumn <= spritePosition + 1)
+        context.screen[pixelRow][pixelColumn] = pixelOn ? '#' : ' '
+
+        if (context.cycles % (6 * 40) === 0) {
+          console.log('Cycle', context.cycles)
+          console.log(gridToString(context.screen))
+        }
+        n++
+      }
+
+      return context
+    }, context)
+  }
+
+  await write(fromHere('solution-part2.md'), [
+    '# Solution to Part 2',
+    'Cycle ' + context.cycles,
+    '',
+    gridToString(context.screen),
+    '',
+    '```',
+    JSON.stringify(context, null, 2),
+    '```'
+  ].join('\n'))
+
+  const solution = '\n' + gridToString(context.screen)
   report('Solution 2:', solution)
 }
 
